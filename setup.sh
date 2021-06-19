741dde55d0738cd808451d8f287491437dc45833
naidubhavesh999 <86152402+naidubhavesh999@users.noreply.github.com>
2021-06-20 00:24:49 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,13 +1,7 @@
-mkdir -p ~/.streamlit/
-
-echo "\
-[general]\n\
-email = \"bhaveshnaidu999@gmail.com\"\n\
-" > ~/.streamlit/credentials.toml
-
-echo "\
+mkdir -p ~/.streamlit/echo "\
 [server]\n\
 headless = true\n\
-enableCORS=false\n\
 port = $PORT\n\
+enableCORS = false\n\
+\n\
 " > ~/.streamlit/config.toml
